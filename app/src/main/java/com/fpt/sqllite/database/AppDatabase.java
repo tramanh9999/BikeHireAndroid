96be022b1546a7a592850f8624abce8c724472a7
tramanh9999 <anhthtse141171@fpt.edu.vn>
2019-07-18 12:49:48 +0700

    add bike deatail xml

--- app/src/main/java/com/fpt/sqllite/database/AppDatabase.java
+++ app/src/main/java/com/fpt/sqllite/database/AppDatabase.java
@@ -2,10 +2,7 @@ package com.fpt.sqllite.database;
 
 import android.content.Context;
 
-import androidx.annotation.NonNull;
 import androidx.room.Database;
-import androidx.room.DatabaseConfiguration;
-import androidx.room.InvalidationTracker;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 

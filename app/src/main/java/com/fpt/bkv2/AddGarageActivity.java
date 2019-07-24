6ef54ded3abf18dabdf6d5ddbedbd933f8275a09
huynhhoang1112 <47128569+huynhhoang1112@users.noreply.github.com>
2019-07-24 23:29:47 +0700

    new layout: GarageMap

--- app/src/main/java/com/fpt/bkv2/AddGarageActivity.java
+++ app/src/main/java/com/fpt/bkv2/AddGarageActivity.java
@@ -1,23 +1,36 @@
 package com.fpt.bkv2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
@@ -32,6 +45,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class AddGarageActivity extends AppCompatActivity {
 
@@ -39,41 +53,116 @@ public class AddGarageActivity extends AppCompatActivity {
     private static final String IMAGE_DIRECTORY = "/demonuts";
     private int GALLERY = 1, CAMERA = 2;
     private Button btnImage;
+    private Button btnGetLocation;
+    private EditText edtLocation;
+    LocationManager locationManager;
+    private static final long MINIMUM_DISTANCE_CHANGE_FOR_UPDATES = 1;
+    private static final long MINIMUM_TIME_BETWEEN_UPDATES = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_garage);
 
         requestMultiplePermissions();
-        btnImage = (Button)findViewById(R.id.btnGarageImg);
+        edtLocation = findViewById(R.id.edtLocation);
+        btnGetLocation = (Button) findViewById(R.id.btnGetLocation);
+        btnImage = (Button) findViewById(R.id.btnGarageImg);
         imageview = (ImageView) findViewById(R.id.imgView);
+
         btnImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showPictureDialog();
             }
         });
+        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1000);
+                } else {
+                    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    try {
+                        String city = hereLocation(location.getLatitude(), location.getLongitude());
+                        edtLocation.setText(city);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                        Toast.makeText(AddGarageActivity.this, "Not Found", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
     }
 
-    private void showPictureDialog(){
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] premissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, premissions, grantResults);
+        switch (requestCode){
+            case 1000:{
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    try {
+                        String city = hereLocation(location.getLatitude(), location.getLongitude());
+                        edtLocation.setText(city);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                        Toast.makeText(AddGarageActivity.this, "Not Found", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(this, "Premission not granted!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+
+    private String hereLocation(double lat, double lon){
+        String cityName = "";
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = geocoder.getFromLocation(lat, lon, 10);
+            if(addresses.size() > 0){
+                for (Address adr: addresses){
+                    if(adr.getLocality() != null && adr.getLocality() .length() > 0){
+                        cityName = adr.getLocality() + ", " + adr.getSubAdminArea() + ", " + adr.getAdminArea() + " / " +  adr.getLatitude() + " - " + adr.getLongitude();
+                        break;
+                    }
+                }
+            }
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+        return cityName;
+    }
+
+
+
+    private void showPictureDialog() {
         AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
         pictureDialog.setTitle("Select Action");
         String[] pictureDialogItems = {
                 "Select photo from gallery",
-                "Capture photo from camera" };
+                "Capture photo from camera"};
         pictureDialog.setItems(pictureDialogItems, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case 0:
-                                choosePhotoFromGallary();
-                                break;
-                            case 1:
-                                takePhotoFromCamera();
-                                break;
-                        }
-                    }
-                });
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        choosePhotoFromGallary();
+                        break;
+                    case 1:
+                        takePhotoFromCamera();
+                        break;
+                }
+            }
+        });
         pictureDialog.show();
     }
 
@@ -148,7 +237,7 @@ public class AddGarageActivity extends AppCompatActivity {
         return "";
     }
 
-    private void  requestMultiplePermissions(){
+    private void requestMultiplePermissions() {
         Dexter.withActivity(this)
                 .withPermissions(
                         Manifest.permission.CAMERA,
@@ -175,4 +264,6 @@ public class AddGarageActivity extends AppCompatActivity {
                 .check();
 
     }
+
+
 }
